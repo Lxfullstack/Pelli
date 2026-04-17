63b7a242dee265d4cc54da5a27a62319792f2c88
Lxfullstack <lx.fullstack@gmail.com>
2026-04-17 15:23:51 +0100

    Catch phrase added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import movies from '../movies'
 import MovieCard from "../components/MovieCard";
 
 const Home = () => {
   return (
     <div className="bg-black min-h-screen p-6">
-        <h1 className="text-white text-2xl font-bold mb-6">
-        Pelli Movies
-       </h1>
+        <div className="mb-6">
+            <h1 className="text-white text-2xl font-bold">
+            Pelli Movies
+            </h1>
+            <p className="text-2 text-amber-50">Welcome to pelli movies "No ads, No delayed downloads"</p>
+        </div>
 
        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {movies.map((movie) => (
