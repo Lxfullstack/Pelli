3987cecf1ddae3ea6e4d9d8426715097c3cbcf31
Lxfullstack <lx.fullstack@gmail.com>
2026-04-17 15:59:16 +0100

    added the dropbox link for avatar the last airbender

--- src/movies.js
+++ src/movies.js
@@ -6,7 +6,7 @@ const movies = [
     title: "The Legend of Aang 2026 (The last airbender)",
     poster: avatar,
     description: "Legend of aang  the last airbender by paramount movies",
-    downloadUrl: ""
+    downloadUrl: "https://www.dropbox.com/scl/fi/58uq2rgbdsjlu3u6owoky/The.Legend.Of.Aang.The.last.Airbender.-THENKIRI.COM-.2026.WEBRip.DOWNLOADED.FROM.THENKIRI.COM.mkv?rlkey=fa0p3cgy93blu6t21n7vfdrnk&st=fpkh1fhe&dl=1"
   }
 ];
 
